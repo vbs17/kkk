13b007ccb76a1e13e6c9de0701a38c73e106b386
kei ikeuchi <sister@sister.local>
2016-11-13 15:05:07 +0900

    m

--- Kiki/IkuTableViewCell.swift
+++ Kiki/IkuTableViewCell.swift
@@ -11,6 +11,8 @@ class IkuTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        imageView?.layer.cornerRadius = imageView!.frame.size.width / 2
+        imageview.clipsToBounds = true
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
