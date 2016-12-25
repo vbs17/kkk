edc690a892fcc3dd344fa35e8beb420efff09e2a
vbs17 <giggsister1996@gmail.com>
2016-12-25 21:48:53 +0900

    s

---
+++ Kiki/imageData.swift
@@ -0,0 +1,13 @@
+//
+//  imageData.swift
+//  Kiki
+//
+//  Created by kei ikeuchi on 2016/12/25.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class imageData: NSObject {
+
+}
