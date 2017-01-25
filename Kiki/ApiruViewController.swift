02492eb1a7d212d4c4bcdf90b7b6d9ab11783451
vbs17 <giggsister1996@gmail.com>
2017-01-25 12:14:55 +0900

    s

---
+++ Kiki/ApiruViewController.swift
@@ -0,0 +1,76 @@
+
+import UIKit
+
+class ApiruViewController: UIViewController {
+
+    
+    @IBOutlet weak var lbl: UILabel!
+    
+    @IBOutlet weak var ori1: UIButton!
+    @IBOutlet weak var cover1: UIButton!
+    @IBOutlet weak var origui1: UIButton!
+    @IBOutlet weak var covgui1: UIButton!
+    @IBOutlet weak var oribas1: UIButton!
+    @IBOutlet weak var covbas1: UIButton!
+    @IBOutlet weak var oridru1: UIButton!
+    @IBOutlet weak var covdru1: UIButton!
+    @IBAction func ori(_ sender: Any) {
+    }
+    @IBAction func cover(_ sender: Any) {
+    }
+    @IBAction func origui(_ sender: Any) {
+    }
+    @IBAction func covgui(_ sender: Any) {
+    }
+    @IBAction func oribas(_ sender: Any) {
+    }
+    @IBAction func covbas(_ sender: Any) {
+    }
+    @IBAction func oridru(_ sender: Any) {
+    }
+    @IBAction func covdru(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ori1.layer.cornerRadius = 2
+        ori1.clipsToBounds = true
+        ori1.layer.borderWidth = 1
+        ori1.layer.borderColor = UIColor.white.cgColor
+        cover1.layer.cornerRadius = 2
+        cover1.clipsToBounds = true
+        cover1.layer.borderWidth = 1
+        cover1.layer.borderColor = UIColor.white.cgColor
+        origui1.layer.cornerRadius = 2
+        origui1.clipsToBounds = true
+        origui1.layer.borderWidth = 1
+        origui1.layer.borderColor = UIColor.white.cgColor
+        covgui1.layer.cornerRadius = 2
+        covgui1.clipsToBounds = true
+        covgui1.layer.borderWidth = 1
+        covgui1.layer.borderColor = UIColor.white.cgColor
+        oribas1.layer.cornerRadius = 2
+        oribas1.clipsToBounds = true
+        oribas1.layer.borderWidth = 1
+        oribas1.layer.borderColor = UIColor.white.cgColor
+        covbas1.layer.cornerRadius = 2
+        covbas1.clipsToBounds = true
+        covbas1.layer.borderWidth = 1
+        covbas1.layer.borderColor = UIColor.white.cgColor
+        oridru1.layer.cornerRadius = 2
+        oridru1.clipsToBounds = true
+        oridru1.layer.borderWidth = 1
+        oridru1.layer.borderColor = UIColor.white.cgColor
+        covdru1.layer.cornerRadius = 2
+        covdru1.clipsToBounds = true
+        covdru1.layer.borderWidth = 1
+        covdru1.layer.borderColor = UIColor.white.cgColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+  
+}
