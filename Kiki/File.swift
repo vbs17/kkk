a0cd59b6f7a3e45698358c5bdc226940b443c0cb
vbs17 <giggsister1996@gmail.com>
2017-01-30 15:10:23 +0900

    m

--- Kiki/File.swift
+++ Kiki/File.swift
@@ -4,15 +4,14 @@ import Foundation
 
 struct CommonConst {
     static let PostPATH = "posts"
+    static let songData = "songData"
+    static let image = "image"
     static let PostPATH2 = "posts2"
+    static let image2 = "image2"
     static let Profile = "Profile"
     static let DisplayNameKey = "DisplayNameKey"
     static let IsSavePlofileData = "IS_SAVE_PLOFILE_DATA"
     static let IsTutorial = "IsTutorial"
     static let IsTutorial2 = "IsTutorial2"
     static let IsTutorial3 = "IsTutorial3"
-    static let songData = "songData"
-    static let image = "image"
-    static let image2 = "image2"
-
 }
