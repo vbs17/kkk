f063b75ea360b34f893da19a52da635463f0f848
vbs17 <giggsister1996@gmail.com>
2017-02-05 10:13:23 +0900

    s

---
+++ Kiki/Home22TableViewCell.swift
@@ -0,0 +1,21 @@
+
+
+import UIKit
+import AVFoundation
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
+
+class Home22TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
