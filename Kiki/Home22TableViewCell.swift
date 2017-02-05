a559ec46afbc733908d5b527d0513b302184275a
vbs17 <giggsister1996@gmail.com>
2017-02-05 10:55:52 +0900

    m

--- Kiki/Home22TableViewCell.swift
+++ Kiki/Home22TableViewCell.swift
@@ -6,10 +6,38 @@ import Firebase
 import FirebaseAuth
 import FirebaseDatabase
 
-
 class Home22TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var pro: UIButton!
+    @IBOutlet weak var pathGo: UIButton!
+    @IBOutlet weak var iku: UIButton!
+    @IBOutlet weak var join: SpringButton!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var ikuhito: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView1: UIImageView!
     
+    func setPostData(_ postData: PostData1, myid: String) {
+        label1.text = postData.hiniti
+        label2.text = postData.zikoku
+        label3.text = postData.station
+        let likeNumber = postData.join.count
+        ikuhito.text = "\(likeNumber)"
+        
+        if postData.isLiked {
+            let buttonImage = UIImage(named: "IMG_2727 2")
+            join.setImage(buttonImage, for: UIControlState())
+        } else {
+            let buttonImage = UIImage(named: "IMG_2728 2")
+            join.setImage(buttonImage, for: UIControlState())
+        }
+    }
     
+    func setPostData1(_ postData: PostData4, myid: String) {
+        ImageView.image = postData.image
+    }
     
     
     
@@ -18,11 +46,18 @@ class Home22TableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        pathGo.layer.cornerRadius = 35
+        pathGo.clipsToBounds = true
+        imageView1.layer.cornerRadius = 22.2
+        imageView1.clipsToBounds = true
+        
     }
-
+    
+    
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
     }
     
 }
