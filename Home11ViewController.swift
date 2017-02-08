061c1e7f2c77c5d5c93f733168c740c3b7e39ee2
vbs17 <giggsister1996@gmail.com>
2017-02-08 23:03:56 +0900

    s

--- Home11ViewController.swift
+++ Home11ViewController.swift
@@ -484,7 +484,13 @@ class Home11ViewController: UIViewController,UITableViewDataSource, UITableViewD
                 
                 timer.invalidate()
                 //ここもポイント
-                SVProgressHUD.setDefaultMaskType(.clear)
+                cell?.playButton.isEnabled = false
+                cell?.hyouka.isEnabled = false
+                cell?.go.isEnabled = false
+
+                
+                //SVProgressHUD.setDefaultMaskType(.clear)
+                
                 
                 //backだけはokにしたいな
                 SVProgressHUD.show(withStatus:"クールな音質に仕上げています😎(最大5秒) 　　　　　　　　　　　　　　　　                　　　　　　このオリジナルソングが君のセンスにあえば左上のProfileボタンをタップして俺に連絡をとってくれ😎あ、あと最高のヘッドホンで聞いてくれよな😜")
@@ -495,6 +501,10 @@ class Home11ViewController: UIViewController,UITableViewDataSource, UITableViewD
                         return
                     }
                     SVProgressHUD.dismiss()
+                    cell?.playButton.isEnabled = true
+                    cell?.hyouka.isEnabled = true
+                    cell?.go.isEnabled = true
+                    
                     if self.presentedViewController != nil {
                         return
                     }
