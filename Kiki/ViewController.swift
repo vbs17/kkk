e95602e95b204188f724a74e17adfe5fb2bed095
vbs17 <giggsister1996@gmail.com>
2017-02-16 21:50:58 +0900

    m

--- Kiki/ViewController.swift
+++ Kiki/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController,AVAudioRecorderDelegate {
     let fileName = "sister.m4a"
     var timer: Timer!
     var timeCountTimer: Timer!
-    let photos = ["fu10","fu9","fu8","fu6","fu7","koi"]
+    let photos = ["fu10","fu9","fu8","fu6","fu7","koi","red3"]
     var count = 1
     var timeCount = 1
     var count1: Bool = false
@@ -24,6 +24,7 @@ class ViewController: UIViewController,AVAudioRecorderDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var viewImage: UIView!
+    @IBOutlet weak var imageView2: UIImageView!
     @IBOutlet weak var recordImage: UIButton?
     @IBOutlet weak var nami1: UIProgressView!
     @IBOutlet weak var nami2: UIProgressView!
@@ -36,7 +37,9 @@ class ViewController: UIViewController,AVAudioRecorderDelegate {
         recordImage!.clipsToBounds = true
         recordImage?.layer.borderWidth = 3
         recordImage?.layer.borderColor = UIColor.black.cgColor
-
+        imageView2.layer.cornerRadius = 7
+        imageView2.clipsToBounds = true
+    
     }
     
     func nextGamenn(){
@@ -138,6 +141,7 @@ class ViewController: UIViewController,AVAudioRecorderDelegate {
     func nextPage (_ sender:Timer){
         
         var image:UIImage! = UIImage(named: photos[1])
+        let image1:UIImage! = UIImage(named: photos[6])
         if count == 1{
             count1 = true
             imageView.image = image;
@@ -161,6 +165,7 @@ class ViewController: UIViewController,AVAudioRecorderDelegate {
             count1 = true
             image = UIImage(named: photos[5])
             imageView.image = image
+            imageView2.image = image1
             sender.invalidate()
             print("デバッグ\(audioRecorder)")
             audioRecorder?.prepareToRecord()
