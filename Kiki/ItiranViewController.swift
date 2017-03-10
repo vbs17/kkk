44eb7458c2ef3ede7da0f70aa24c649ed0c63ef5
vbs17 <giggsister1996@gmail.com>
2017-03-10 09:37:45 +0900

    m

--- Kiki/ItiranViewController.swift
+++ Kiki/ItiranViewController.swift
@@ -479,17 +479,16 @@ class ItiranViewController: UIViewController, UITableViewDelegate, UITableViewDa
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let cell = tableVView.dequeueReusableCell(withIdentifier: "Celll") as! ItiranTableViewCell
-
-            for post in cell{
+        for post in cell{
             if (post.label.text == genre1) {
-            if(shine == true){
-                cell.imageViewVV.backgroundColor = UIColor.red
-            }else{
-                cell.imageViewVV.backgroundColor = UIColor.white
+                if(shine == true){
+                    cell.imageViewVV.backgroundColor = UIColor.red
+                }else{
+                    cell.imageViewVV.backgroundColor = UIColor.white
+                }
+                
             }
-        
         }
-    }
     }
     
     
